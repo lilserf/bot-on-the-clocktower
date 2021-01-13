dde6275025a51e972568893300b72d57da8e6d6f
Mark Haines <lilserf@gmail.com>
2021-01-12 23:19:40 -0500

    Fixes for issue #1, #2, #4, and various other tweaks and cleanup

--- bot.py
+++ bot.py
@@ -43,6 +43,13 @@ def getInfo(ctx):
     d['storytellerRole'] = discord.utils.find(lambda r: r.name==CURRENT_STORYTELLER, guild.roles)
     d['currentPlayerRole'] = discord.utils.find(lambda r: r.name==CURRENT_GAME, guild.roles)
 
+    activePlayers = set()
+    for c in d['dayChannels']:
+        activePlayers.update(c.members)
+    for c in d['nightChannels']:
+        activePlayers.update(c.members)
+    d['activePlayers'] = activePlayers
+
     return d
 
 @bot.event
@@ -53,10 +60,20 @@ async def on_ready():
 def userNames(users):
     return list(map(lambda x: x.display_name, users))
 
+# Helper to see if a command context is in a valid channel etc - used by all the commands
+async def isValid(ctx):
+    if isinstance(ctx.channel, discord.TextChannel):
+        if ctx.channel.name == CONTROL_CHANNEL:
+            return True
+    elif isinstance(ctx.channel, discord.DMChannel):
+        await ctx.send(f"Whoops, you probably meant to send that in the `{CONTROL_CHANNEL}` channel instead! Sorry, mate.")
+    return False
+
+
 # Set the players in the normal voice channels to have the 'Current Game' role, granting them access to whatever that entails
 @bot.command(name='currgame', help='Set the current users in all standard BotC voice channels as players in a current game, granting them roles to see channels associated with the game.')
 async def onCurrGame(ctx):
-    if ctx.channel.name != CONTROL_CHANNEL:
+    if not await isValid(ctx):
         return
 
     try:
@@ -73,11 +90,7 @@ async def onCurrGame(ctx):
         role = info['currentPlayerRole']
 
         # find all users currently in the channels we play in
-        currPlayers = set()
-        for c in info['dayChannels']:
-            currPlayers.update(c.members)
-        for c in info['nightChannels']:
-            currPlayers.update(c.members)
+        currPlayers = info['activePlayers']
 
         # find additions and deletions by diffing the sets
         remove = prevPlayers - currPlayers
@@ -100,31 +113,25 @@ async def onCurrGame(ctx):
             await ctx.send(addMsg)
 
     except Exception as ex:
-        await sendErrorToAuthor(ctx, repr(ex))
+        await sendErrorToAuthor(ctx)
 
 # Given a list of users and a name string, find the user with the closest name
 def getClosestUser(userlist, name):
     for u in userlist:
-        # Try an exact match
-        if u.display_name.lower() == name.lower():
+        # See if anybody's name starts with what was sent
+        if u.display_name.lower().startswith(name.lower()):
             return u
-        else:
-            # Okay, try an exact match of the first word
-            splits = u.display_name.split(' ')
-
-            if splits[0].lower() == name.lower():
-                return u
-            else:
-                # Okay, try the first word *starting* with the string
-                if splits[0].lower().startswith(name.lower()):
-                    return u
     
     return None
 
 # Helper to send a message to the author of the command about what they did wrong
-async def sendErrorToAuthor(ctx, error):
-    formatted = traceback.format_exc(3)
-    await ctx.author.send(f"{formatted}\n(from message `{ctx.message.content}`)")
+async def sendErrorToAuthor(ctx, error=None):
+    if error is not None:
+        formatted = error
+    else:
+        formatted = '```\n' + traceback.format_exc(3) + '\n```'
+        traceback.print_exc()
+    await ctx.author.send(f"Alas, an error has occurred:\n{formatted}\n(from message `{ctx.message.content}`)")
 
 # Common code for parsing !evil and !lunatic commands
 async def processMessage(ctx, users):
@@ -142,14 +149,18 @@ async def processMessage(ctx, users):
     demonUser = getClosestUser(users, demon)
     minionUsers = list(map(lambda x: getClosestUser(users, x), minions))
 
+    info = getInfo(ctx)
+    categories = [info['dayCategory'].name, info['nightCategory'].name]
+    catString = ', '.join(categories)
+
     # Error messages for users not found
     if demonUser is None:
-        await sendErrorToAuthor(ctx, f"Unknown user '{demon}'.")
+        await sendErrorToAuthor(ctx, f"Couldn't find user **{demon}** in these categories: {catString}.")
         return (False, None, None)
 
     for (i, m) in enumerate(minionUsers):
         if m is None:
-            await sendErrorToAuthor(ctx, f"Unknown user '{minions[i]}'.")
+            await sendErrorToAuthor(ctx, f"Couldn't find user **{minions[i]}** in these categories: {catString}.")
             return (False, None, None)
 
     return (True, demonUser, minionUsers)
@@ -165,12 +176,12 @@ async def sendDemonMessage(demonUser, minionUsers):
 # Works the same as !evil, but doesn't message the minions
 @bot.command(name='lunatic', help='Send fake evil info to the Lunatic. Format is `!lunatic <Lunatic> <fake minion> <fake minion> <fake minion>`')
 async def onLunatic(ctx):
-    if ctx.channel.name != CONTROL_CHANNEL:
+    if not await isValid(ctx):
         return
 
     try:
         info = getInfo(ctx)
-        users = info['townSquare'].members
+        users = info['activePlayers']
         (success, demonUser, minionUsers) = await processMessage(ctx, users)
 
         if not success:
@@ -179,17 +190,17 @@ async def onLunatic(ctx):
         await sendDemonMessage(demonUser, minionUsers)
 
     except Exception as ex:
-        await sendErrorToAuthor(ctx, repr(ex))
+        await sendErrorToAuthor(ctx)
 
 # Command to send demon/minion info to the Demon and Minions
 @bot.command(name='evil', help='Send evil info to evil team. Format is `!evil <demon> <minion> <minion> <minion>`')
 async def onEvil(ctx):
-    if ctx.channel.name != CONTROL_CHANNEL:
+    if not await isValid(ctx):
         return
 
     try:
         info = getInfo(ctx)
-        users = info['townSquare'].members
+        users = info['activePlayers']
         (success, demonUser, minionUsers) = await processMessage(ctx, users)
 
         if not success:
@@ -197,7 +208,10 @@ async def onEvil(ctx):
 
         await sendDemonMessage(demonUser, minionUsers)
 
-        minionMsg = "{}: You are a **minion**. Your demon is: {}. Your fellow minions are: {}."
+        minionMsg = "{}: You are a **minion**. Your demon is: {}."
+        
+        if len(minionUsers) > 1:
+            minionMsg += " Your fellow minions are: {}."
 
         for m in minionUsers:
             otherMinions = userNames(minionUsers)
@@ -210,12 +224,12 @@ async def onEvil(ctx):
         await ctx.send("The Evil team has been informed...")
             
     except Exception as ex:
-        await sendErrorToAuthor(ctx, repr(ex))
+        await sendErrorToAuthor(ctx)
 
 # Move users to the night cottages
 @bot.command(name='night', help='Move users to Cottages in the BotC - Nighttime category')
 async def onNight(ctx):
-    if ctx.channel.name != CONTROL_CHANNEL:
+    if not await isValid(ctx):
         return
 
     try:
@@ -233,7 +247,7 @@ async def onNight(ctx):
         await storyteller.add_roles(role)
 
         # get list of users in town square   
-        users = info['townSquare'].members
+        users = info['activePlayers']
         cottages = list(info['nightChannels'])
         cottages.sort(key=lambda x: x.position)
 
@@ -242,19 +256,19 @@ async def onNight(ctx):
 
         # move each user to a cottage
         for (user, cottage) in sorted(pairs, key=lambda x: x[0].name):
-            print("Moving %s to %s %d" % (user.name, cottage.name, cottage.id))
+            # print("Moving %s to %s %d" % (user.name, cottage.name, cottage.id))
             # remove the Current Storyteller role from other folks we're moving
             if user.id != storyteller.id:
                 await user.remove_roles(role)
             await user.move_to(cottage)
 
     except Exception as ex:
-        await sendErrorToAuthor(ctx, repr(ex))
+        await sendErrorToAuthor(ctx)
 
 # Move users from night Cottages back to Town Square
 @bot.command(name='day', help='Move users from Cottages back to Town Square')
 async def onDay(ctx):
-    if ctx.channel.name != CONTROL_CHANNEL:
+    if not await isValid(ctx):
         return
 
     try:
@@ -272,12 +286,12 @@ async def onDay(ctx):
             await user.move_to(info['townSquare'])
 
     except Exception as ex:
-        await sendErrorToAuthor(ctx, repr(ex))
+        await sendErrorToAuthor(ctx)
 
 # Move users from other daytime channels back to Town Square
 @bot.command(name='vote', help='Move users from other channels back to Town Square')
 async def onVote(ctx):
-    if ctx.channel.name != CONTROL_CHANNEL:
+    if not await isValid(ctx):
         return
 
     try:
@@ -296,7 +310,7 @@ async def onVote(ctx):
             await user.move_to(info['townSquare'])
     
     except Exception as ex:
-        await sendErrorToAuthor(ctx, repr(ex))
+        await sendErrorToAuthor(ctx)
 
 
 bot.run(TOKEN)
