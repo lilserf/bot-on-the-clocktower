9f2345697d9b42d67740412c9a7af533d0e32bbd
Mark Haines <lilserf@gmail.com>
2021-03-30 01:11:48 -0400

    Massive refactor of getInfo into a more-typed TownInfo class which is now driven by the stored channels/roles/etc in MongoDb

--- bot.py
+++ bot.py
@@ -29,23 +29,17 @@ bot = commands.Bot(command_prefix='!', intents=intents, description='Bot to move
 # Send Messages, Manage Messages
 # Move Members
 
-
-DAY_CATEGORY = 'BotC - Daytime'
-NIGHT_CATEGORY = 'BotC - Nighttime'
-TOWN_SQUARE = 'Town Square'
-CONTROL_CHANNEL = 'botc_mover'
-CURRENT_STORYTELLER = 'BotC Current Storyteller'
-CURRENT_GAME = 'BotC Current Game'
-
+################
+# Connect to mongo and get our DB object used globally throughout this file
 MONGO_CONNECT = os.getenv('MONGO_CONNECT')
 cluster = MongoClient(MONGO_CONNECT)
 db = cluster['botc']
 
 guildInfo = db['GuildInfo']
 
-
-def getChannelByName(guild, name):
-    return discord.utils.find(lambda c: (c.type == discord.ChannelType.voice or c.type == discord.ChannelType.text) and c.name == name, guild.channels)
+# Helpers
+def getChannelFromCategoryByName(category, name):
+    return discord.utils.find(lambda c: (c.type == discord.ChannelType.voice or c.type == discord.ChannelType.text) and c.name == name, category.channels)
 
 def getCategoryByName(guild, name):
     return discord.utils.find(lambda c: c.type == discord.ChannelType.category and c.name == name, guild.channels)
@@ -53,45 +47,134 @@ def getCategoryByName(guild, name):
 def getRoleByName(guild, name):
     return discord.utils.find(lambda r: r.name==name, guild.roles)
 
-@bot.command(name='addTown', help='Add a game on this server')
-async def addTown(ctx):
+# Parse all the params for addTown, sanity check them, and return useful dicts
+async def resolveTownInfo(ctx, params):
+    
     guild = ctx.guild
-
-    params = shlex.split(ctx.message.content)
 
     if len(params) < 7:
         await ctx.send("Too few params to `!addTown`: should provide `<control channel> <townsquare channel> <day category> <night category> <ST role> <player role>`")
 
-    print(params)
+    dayCatName = params[3]
+    nightCatName = params[4]
 
-    post = {"guild" : guild.id,
-        "controlChannel" : getChannelByName(guild, params[1]).id,
-        "townSquare" : getChannelByName(guild, params[2]).id,
-        "dayCategory" : getCategoryByName(guild, params[3]).id,
-        "nightCategory" : getCategoryByName(guild, params[4]).id,
-        "storyTellerRole" : getRoleByName(guild, params[5]).id,
-        "villagerRole" : getRoleByName(guild, params[6]).id
-    }
+    dayCat = getCategoryByName(guild, dayCatName)
+    nightCat = getCategoryByName(guild, nightCatName)
 
-    print(post)
+    controlName = params[1]
+    townSquareName = params[2]
 
-    # TODO: sanity check the provided params
-    # is the town square channel in the day category?
-    # is the control channel in the day category?
+    controlChan = getChannelFromCategoryByName(dayCat, controlName)
+    townSquare = getChannelFromCategoryByName(dayCat, townSquareName)
+
+    stRoleName = params[5]
+    villagerName = params[6]
+
+    stRole = getRoleByName(guild, stRoleName)
+    villagerRole = getRoleByName(guild, villagerName)
+
+    # TODO: more checks
     # does the night category contain some channels to distribute people to?
     
     # TODO: also specify the roles to set
     # does the storyteller role have perms to see all the cottages?
     # do the player roles NOT have perms to see the cottages
 
-    # TODO: convert all these strings to actual channel IDs etc?
+    if not dayCat:
+        await ctx.send(f'Couldn\'t find a category named `{dayCatName}`!')
+        return None
 
+    if not nightCat:
+        await ctx.send(f'Couldn\'t find a category named `{nightCatName}`!')
+        return None
+
+    if not controlChan:
+        await ctx.send(f'Couldn\'t find a channel named `{controlName}` in category `{dayCatName}`!')
+        return None
+
+    if not townSquare:
+        await ctx.send(f'Couldn\'t find a channel named `{townSquareName}` in category `{dayCatName}`!')
+        return None
+
+    if not stRole:
+        await ctx.send(f'Couldn\'t find a role named `{stRoleName}`!')
+        return None
+
+    if not villagerRole:
+        await ctx.send(f'Couldn\'t find a role named `{villagerName}`!')
+        return None
+
+    # Object suitable for mongo
+    post = {   
+        "guild" : guild.id,
+        "controlChannel" : controlName,
+        "controlChannelId" : controlChan.id,
+        "townSquare" : townSquareName,
+        "townSquareId" : townSquare.id,
+        "dayCategory" : dayCatName,
+        "dayCategoryId" : dayCat.id,
+        "nightCategory" : nightCatName,
+        "nightCategoryId" : nightCat.id,
+        "storyTellerRole" : stRoleName,
+        "storyTellerRoleId" : stRole.id,
+        "villagerRole" : villagerName,
+        "villagerRoleId" : villagerRole.id,
+        "authorName" : ctx.author.display_name,
+        "author" : ctx.author.id,
+        "timestamp" : ctx.message.created_at
+    }
+
+    objs = TownInfo(ctx, post)
+
+    return (post, objs)
+
+async def sendEmbed(ctx, townInfo):
+    guild = ctx.guild
+    embed = discord.Embed(title=f'{guild.name} // {townInfo.dayCategory.name}', description=f'Created {townInfo.timestamp} by {townInfo.authorName}', color=0xcc0000)
+    embed.add_field(name="Control Channel", value=townInfo.controlChannel.name, inline=False)
+    embed.add_field(name="Town Square", value=townInfo.townSquare.name, inline=False)
+    embed.add_field(name="Day Category", value=townInfo.dayCategory.name, inline=False)
+    embed.add_field(name="Night Category", value=townInfo.nightCategory.name, inline=False)
+    embed.add_field(name="Storyteller Role", value=townInfo.storyTellerRole.name, inline=False)
+    embed.add_field(name="Villager Role", value=townInfo.villagerRole.name, inline=False)
+    await ctx.send(embed=embed)
+
+
+@bot.command(name='townInfo', aliases=['towninfo'], help='Show the stored info about the channels and roles that make up this town')
+async def townInfo(ctx):
+
+    info = getTownInfo(ctx)
+
+    await sendEmbed(ctx, info)
+
+
+@bot.command(name='addTown', help='Add a game on this server')
+async def addTown(ctx):
+    guild = ctx.guild
+
+    params = shlex.split(ctx.message.content)
+
+    (post, info) = await resolveTownInfo(ctx, params)
+
+    if not post:
+        return
+
+    # Check if a town already exists
+    query = { 
+        "guild" : post["guild"],
+        "dayCategoryId" : post["dayCategoryId"],
+    }
+
+    existing = guildInfo.find_one(query)
+
+    if existing:
+        await ctx.send(f'Found an existing town on this server using daytime category `{post["dayCategory"]}`, modifying it!')
+
+    # Upsert the town into place
     print(f'Adding a town to guild {post["guild"]} with control channel [{post["controlChannel"]}], day category [{post["dayCategory"]}], night category [{post["nightCategory"]}]')
-    guildInfo.insert_one(post)
+    guildInfo.replace_one(query, post, True)
 
-    # TODO: send a confirmation message about everything we just saved
-    # TODO: run some kind of test, maybe?
-
+    await sendEmbed(ctx, info)
 
 
 @bot.command(name='removeTown', help='Remove a game on this server')
@@ -107,37 +190,44 @@ async def removeTown(ctx):
     guildInfo.delete_one(post)
 
 
-# Grab a bunch of common info we need from this particular server,
-# based on the assumptions we make about servers
-def getInfo(ctx):
-    guild = ctx.guild
+# Nicer than a dict for storing info about the town
+class TownInfo:
+    def __init__(self, ctx, document):
+        guild = ctx.guild
 
-    post={"guild" : guild.id,
-        "controlChannel" : ctx.channel.id
-    }
+        if document:
+            self.dayCategory = getCategoryByName(guild, document["dayCategory"])
+            self.nightCategory = getCategoryByName(guild, document["nightCategory"])
+            self.townSquare = getChannelFromCategoryByName(self.dayCategory, document["townSquare"])
+            self.controlChannel = getChannelFromCategoryByName(self.dayCategory, document["controlChannel"])
 
-    guildinfo.find_one()
+            self.dayChannels = list(c for c in guild.channels if c.type == discord.ChannelType.voice and c.category_id ==  self.dayCategory.id)
+            self.nightChannels = list(c for c in guild.channels if c.type == discord.ChannelType.voice and c.category_id ==  self.nightCategory.id)
 
-    d = dict()
+            self.storyTellerRole = getRoleByName(guild, document["storyTellerRole"])
+            self.villagerRole = getRoleByName(guild, document["villagerRole"])
 
-    d['dayCategory'] = discord.utils.find(lambda c: c.type == discord.ChannelType.category and c.name == DAY_CATEGORY, guild.channels)
-    d['nightCategory'] = discord.utils.find(lambda c: c.type == discord.ChannelType.category and c.name == NIGHT_CATEGORY, guild.channels)
-    d['townSquare'] = discord.utils.find(lambda c: c.type == discord.ChannelType.voice and c.name == TOWN_SQUARE, guild.channels)
+            activePlayers = set()
+            for c in self.dayChannels:
+                activePlayers.update(c.members)
+            for c in self.nightChannels:
+                activePlayers.update(c.members)
+            self.activePlayers = activePlayers
 
-    d['dayChannels'] = list(c for c in guild.channels if c.type == discord.ChannelType.voice and c.category_id ==  d['dayCategory'].id)
-    d['nightChannels'] = list(c for c in guild.channels if c.type == discord.ChannelType.voice and c.category_id == d['nightCategory'].id)
+            self.authorName = document["authorName"]
+            self.timestamp = document["timestamp"]
 
-    d['storytellerRole'] = discord.utils.find(lambda r: r.name==CURRENT_STORYTELLER, guild.roles)
-    d['currentPlayerRole'] = discord.utils.find(lambda r: r.name==CURRENT_GAME, guild.roles)
 
-    activePlayers = set()
-    for c in d['dayChannels']:
-        activePlayers.update(c.members)
-    for c in d['nightChannels']:
-        activePlayers.update(c.members)
-    d['activePlayers'] = activePlayers
+# Get a well-defined TownInfo based on the stored DB info for this guild
+def getTownInfo(ctx):
 
-    return d
+    query = { "guild" : ctx.guild.id, "controlChannelId" : ctx.channel.id }
+    doc = guildInfo.find_one(query)
+
+    if doc:
+        return TownInfo(ctx, doc)
+    else:
+        return None
 
 @bot.event
 async def on_ready():
@@ -149,15 +239,20 @@ def userNames(users):
 
 # Helper to see if a command context is in a valid channel etc - used by all the commands
 async def isValid(ctx):
+
+    query = { "guild" : ctx.guild.id }
+    result = guildInfo.find(query)
+    chanIds = map(lambda x: x["controlChannelId"], result)
+
     if isinstance(ctx.channel, discord.TextChannel):
-        if ctx.channel.name == CONTROL_CHANNEL:
+        if ctx.channel.id in chanIds:
             return True
     elif isinstance(ctx.channel, discord.DMChannel):
-        await ctx.send(f"Whoops, you probably meant to send that in the `{CONTROL_CHANNEL}` channel instead! Sorry, mate.")
+        await ctx.send(f"Whoops, you probably meant to send that in a text channel instead of a DM! Sorry, mate.")
     return False
 
 # End the game and remove all the roles, permissions, etc
-@bot.command(name='endgame', help='End the current game and reset all permissions, roles, names, etc.')
+@bot.command(name='endGame', aliases=['endgame'], help='End the current game and reset all permissions, roles, names, etc.')
 async def onEndGame(ctx):
     if not await isValid(ctx):
         return
@@ -165,42 +260,42 @@ async def onEndGame(ctx):
     try:
         guild = ctx.guild
 
-        info = getInfo(ctx)
-        playerRole = info['currentPlayerRole']
-        stRole = info['storytellerRole']
+        info = getTownInfo(ctx)
 
         # find all guild members with the Current Game role
         prevPlayers = set()
         prevSt = None
         for m in guild.members:
-            if playerRole in m.roles:
+            if info.villagerRole in m.roles:
                 prevPlayers.add(m)
-            if stRole in m.roles:
+            if info.storyTellerRole in m.roles:
                 prevSt = m
 
         # remove game role from players
         for m in prevPlayers:
-            await m.remove_roles(playerRole)
+            await m.remove_roles(info.villagerRole)
 
         # remove cottage permissions
-        for c in info['nightChannels']:
+        for c in info.nightChannels:
             # Take away permission overwrites for this cottage
             for m in prevPlayers:
                 await c.set_permissions(m, overwrite=None)
-            await c.set_permissions(prevSt, overwrite=None)
+            if prevSt:
+                await c.set_permissions(prevSt, overwrite=None)
 
-        # remove storyteller role and name from storyteller
-        await prevSt.remove_roles(stRole)
-        if prevSt.display_name.startswith('(ST) '):
-            newnick = m.display_name[5:]
-            await m.edit(nick=newnick)
+        if prevSt:
+            # remove storyteller role and name from storyteller
+            await prevSt.remove_roles(info.storyTellerRole)
+            if prevSt.display_name.startswith('(ST) '):
+                newnick = m.display_name[5:]
+                await m.edit(nick=newnick)
 
     except Exception as ex:
         await sendErrorToAuthor(ctx)
 
 
 # Set the players in the normal voice channels to have the 'Current Game' role, granting them access to whatever that entails
-@bot.command(name='currgame', help='Set the current users in all standard BotC voice channels as players in a current game, granting them roles to see channels associated with the game.')
+@bot.command(name='currGame', aliases=['currgame', 'curgame', 'curGame'], help='Set the current users in all standard BotC voice channels as players in a current game, granting them roles to see channels associated with the game.')
 async def onCurrGame(ctx):
     if not await isValid(ctx):
         return
@@ -208,30 +303,22 @@ async def onCurrGame(ctx):
     try:
         guild = ctx.guild
 
+        info = getTownInfo(ctx)
+
         # find all guild members with the Current Game role
         prevPlayers = set()
         for m in guild.members:
-            for r in m.roles:
-                if r.name == CURRENT_GAME:
-                    prevPlayers.add(m)
-
-        info = getInfo(ctx)
-        role = info['currentPlayerRole']
+            if info.villagerRole in m.roles:
+                prevPlayers.add(m)
 
         # grant the storyteller the Current Storyteller role
-        storyteller = ctx.message.author
-        strole = info['storytellerRole']
-        await storyteller.add_roles(strole)
-
-        # find all users currently in the channels we play in
-        currPlayers = info['activePlayers']
-
         storyTeller = ctx.message.author
+        await storyTeller.add_roles(info.storyTellerRole)
 
         # take any (ST) off of old storytellers
-        for o in currPlayers:
-            if o != storyteller and strole in o.roles:
-                await o.remove_roles(strole)
+        for o in info.activePlayers:
+            if o != storyTeller and info.storyTellerRole in o.roles:
+                await o.remove_roles(info.storyTellerRole)
             if o != storyTeller and o.display_name.startswith('(ST) '):
                 newnick = o.display_name[5:]
                 await o.edit(nick=newnick)
@@ -241,23 +328,23 @@ async def onCurrGame(ctx):
             await storyTeller.edit(nick=f"(ST) {storyTeller.display_name}")
 
         # find additions and deletions by diffing the sets
-        remove = prevPlayers - currPlayers
-        add = currPlayers - prevPlayers
+        remove = prevPlayers - info.activePlayers
+        add = info.activePlayers - prevPlayers
 
         # remove any stale players
         if len(remove) > 0:
-            removeMsg = f"Removed {role.name} role from: "
+            removeMsg = f"Removed {info.villagerRole.name} role from: "
             removeMsg += ', '.join(userNames(remove))
             for m in remove:
-                await m.remove_roles(role)
+                await m.remove_roles(info.villagerRole)
             await ctx.send(removeMsg)
 
         # add any new players
         if len(add) > 0:
-            addMsg = f"Added {role.name} role to: "
+            addMsg = f"Added {info.villagerRole.name} role to: "
             addMsg += ', '.join(userNames(add))
             for m in add:
-                await m.add_roles(role)
+                await m.add_roles(info.villagerRole)
             await ctx.send(addMsg)
 
     except Exception as ex:
@@ -301,8 +388,8 @@ async def processMessage(ctx, users):
     demonUser = getClosestUser(users, demon)
     minionUsers = list(map(lambda x: getClosestUser(users, x), minions))
 
-    info = getInfo(ctx)
-    categories = [info['dayCategory'].name, info['nightCategory'].name]
+    info = getTownInfo(ctx)
+    categories = [info.dayCategory.name, info.nightCategory.name]
     catString = ', '.join(categories)
 
     # Error messages for users not found
@@ -332,8 +419,8 @@ async def onLunatic(ctx):
         return
 
     try:
-        info = getInfo(ctx)
-        users = info['activePlayers']
+        info = getTownInfo(ctx)
+        users = info.activePlayers
         (success, demonUser, minionUsers) = await processMessage(ctx, users)
 
         if not success:
@@ -351,8 +438,8 @@ async def onEvil(ctx):
         return
 
     try:
-        info = getInfo(ctx)
-        users = info['activePlayers']
+        info = getTownInfo(ctx)
+        users = info.activePlayers
         (success, demonUser, minionUsers) = await processMessage(ctx, users)
 
         if not success:
@@ -391,12 +478,12 @@ async def onNight(ctx):
         await ctx.send('Moving users to Cottages!')
         
         # get channels we care about
-        info = getInfo(ctx)
+        info = getTownInfo(ctx)
 
         # get list of users in town square   
-        users = list(info['activePlayers'])
+        users = list(info.activePlayers)
         users.sort(key=lambda x: x.display_name)
-        cottages = list(info['nightChannels'])
+        cottages = list(info.nightChannels)
         cottages.sort(key=lambda x: x.position)
 
         # pair up users with cottages
@@ -422,11 +509,11 @@ async def onDay(ctx):
     try:
         await ctx.send('Moving users from Cottages to Town Square.')
 
-        info = getInfo(ctx)
+        info = getTownInfo(ctx)
 
         # get users in night channels
         users = list()
-        for c in info['nightChannels']:
+        for c in info.nightChannels:
             users.extend(c.members)
             # Take away permission overwrites for their cottage
             for m in c.members:
@@ -437,7 +524,7 @@ async def onDay(ctx):
 
         # move them to Town Square
         for user in users:
-            await user.move_to(info['townSquare'])
+            await user.move_to(info.townSquare)
 
     except Exception as ex:
         await sendErrorToAuthor(ctx)
@@ -451,17 +538,17 @@ async def onVote(ctx):
     try:
         await ctx.send('Moving users from other areas to Town Square.')
 
-        info = getInfo(ctx)
+        info = getTownInfo(ctx)
         
         # get users in day channels other than Town Square
         users = list()
-        for c in info['dayChannels']:
-            if c != info['townSquare']:
+        for c in info.dayChannels:
+            if c != info.townSquare:
                 users.extend(c.members)
 
         # move them to Town Square
         for user in users:
-            await user.move_to(info['townSquare'])
+            await user.move_to(info.townSquare)
     
     except Exception as ex:
         await sendErrorToAuthor(ctx)
