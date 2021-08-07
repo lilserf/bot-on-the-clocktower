ad5cf6ef533845f44e16e36b0f7a73b5c961a4ee
Colin C <magroader@gmail.com>
2021-08-07 19:52:05 -0400

    If the lookup has real data, fuzzy string matches like a pro

--- lookup.py
+++ lookup.py
@@ -1,44 +1,32 @@
-import json
-import shlex
 import aiohttp
 import asyncio
-
+import datetime
 from fuzzywuzzy import process
+import json
+import shlex
+
 
 class LookupRole:
-    def is_valid_role_json(json):
-        return json != None and isinstance(json, dict) and 'id' in json and json['id'] != '_meta' and 'name' in json and 'team' in json and 'ability' in json
-
-    def create_role_from_json(json):
-        if LookupRole.is_valid_role_json(json):
-            return LookupRole(json)
-        return None
-
     def matches_other(self, other):
         return self.name == other.name and self.team == other.team and self.ability == other.ability
 
-    def __init__(self, json):
-        self.name = json['name']
-        self.team = json['team']
-        self.ability = json['ability']
+    def __init__(self, name, ability, team):
+        self.name = name
+        self.team = ability
+        self.ability = team
 
 
-class Lookup:
-    def find_role_from_message_content(self, content):
-        return " ".join(shlex.split(content)[1:])
+class LookupRoleParser:
+    def is_valid_role_json(self, json):
+        return json != None and isinstance(json, dict) and 'id' in json and json['id'] != '_meta' and 'name' in json and 'team' in json and 'ability' in json
 
-    async def fetch_url(self, url, session):
-        async with session.get(url) as response:
-            try:
-                return await response.json()
-            except Exception:
-                return []
-
-    async def fetch_urls(self, urls):
-        loop = asyncio.get_event_loop()
-        async with aiohttp.ClientSession() as session:
-            tasks = [loop.create_task(self.fetch_url(url, session)) for url in urls]
-            return await asyncio.gather(*tasks)
+    def create_role_from_json(self, json):
+        if self.is_valid_role_json(json):
+            name = json['name']
+            team = json['team']
+            ability = json['ability']
+            return LookupRole(name, ability, team)
+        return None
 
     def combine_or_append_role(self, role, roleList):
         found = False
@@ -55,27 +43,72 @@ class Lookup:
         else:
             roles[role.name] = [ role ]
 
-    def collect_roles_for_set(self, set, roles):
+    def collect_roles_for_set_json(self, set, roles):
         for json in set:
-            role = LookupRole.create_role_from_json(json)
+            role = self.create_role_from_json(json)
             if role != None:
                  self.add_role(role, roles)
 
-    def collect_roles_from_results(self, results):
+    def collect_roles_from_json(self, results):
         roles = {}
         for set in results:
             if isinstance(set, list):
-                self.collect_roles_for_set(set, roles)
+                self.collect_roles_for_set_json(set, roles)
         return roles
 
 
-    async def collect_all_roles(self):
-        urls = [
-            'https://www.bloodstar.xyz/p/MagRoader/StarWars/script.json',
-            'https://www.bloodstar.xyz/p/morilac/Pandemonium/script.json',
-        ]
+class LookupRoleDownloader:
+    def find_role_from_message_content(self, content):
+        return " ".join(shlex.split(content)[1:])
+
+    async def fetch_url(self, url, session):
+        async with session.get(url) as response:
+            try:
+                return await response.json()
+            except Exception:
+                return []
+
+    async def fetch_urls(self, urls):
+        loop = asyncio.get_event_loop()
+        async with aiohttp.ClientSession() as session:
+            tasks = [loop.create_task(self.fetch_url(url, session)) for url in urls]
+            return await asyncio.gather(*tasks)
+
+
+    async def collect_roles_from_urls(self, urls):
         results = await self.fetch_urls(urls)
-        return collect_roles_from_results(results)
+        parser = LookupRoleParser()
+        return parser.collect_roles_from_json(results)
+
+
+class LookupRoleServerData:
+    def __init__(self, role_lookup):
+        self.role_lookup = role_lookup
+        self.last_refresh_time = datetime.datetime.now()
+
+    def get_matching_roles(self, role_name):
+        option = process.extractOne(role_name, self.role_lookup.keys(), score_cutoff=80)
+        if option != None:
+            return self.role_lookup[option[0]]
+        return None
+
+
+class LookupRoleDatabase:
+    def __init__(self):
+        self.server_data = {}
+
+    def update_server_data(self, server_token, role_lookup):
+        self.server_data[server_token] = LookupRoleServerData(role_lookup)
+
+    def server_needs_update(self, server_token):
+        return (not server_token in self.server_data) or self.server_data[server_token].last_refresh_time == None or (datetime.datetime.now() - self.server_data[server_token].last_refresh_time) > datetime.timedelta(days=1)
+
+
+class Lookup:
+        #urls = [
+        #    'https://www.bloodstar.xyz/p/MagRoader/StarWars/script.json',
+        #    'https://www.bloodstar.xyz/p/morilac/Pandemonium/script.json',
+        #]
 
 
     async def role_lookup(self, ctx):
--- test_lookups.py
+++ test_lookups.py
@@ -5,60 +5,105 @@ import lookup
 class TestLookups(unittest.TestCase):
 
     def test_coolectsroles_norolejson_createsnone(self):
-        lk = lookup.Lookup()
+        parser = lookup.LookupRoleParser()
 
         results_json = [[]]
-        roles = lk.collect_roles_from_results(results_json)
+        roles = parser.collect_roles_from_json(results_json)
 
-        self.assertEqual(len(roles), 0, "Expected 0 roles")
+        self.assertEqual(len(roles), 0, 'Expected 0 roles')
 
 
     def test_coolectsroles_novalidrolejson_createsnone(self):
-        
-        lk = lookup.Lookup()
+        parser = lookup.LookupRoleParser()
 
         results_json = [[{'id':'role-id', 'name':'Has Name', 'ability':'has no team'}]]
-        roles = lk.collect_roles_from_results(results_json)
+        roles = parser.collect_roles_from_json(results_json)
 
-        self.assertEqual(len(roles), 0, "Expected 0 roles")
+        self.assertEqual(len(roles), 0, 'Expected 0 roles')
 
 
-    def test_coolectsroles_onerole_createsone(self):
-        
-        lk = lookup.Lookup()
+    def test_coolectsroles_onerole_createsone(self):        
+        parser = lookup.LookupRoleParser()
 
         results_json = [[{'id':'role-id', 'name':'Has Name', 'ability':'some ability', 'team':'Townsfolk'}]]
-        roles = lk.collect_roles_from_results(results_json)
+        roles = parser.collect_roles_from_json(results_json)
 
-        self.assertEqual(len(roles), 1, "Expected 1 role")
+        self.assertEqual(len(roles), 1, 'Expected 1 role')
 
 
     def test_coolectsroles_manyroles_createsall(self):
-        
-        lk = lookup.Lookup()
+        parser = lookup.LookupRoleParser()
 
         results_json = [[
             {'id':'role-id-1', 'name':'Name 1', 'ability':'some ability', 'team':'Townsfolk'},
             {'id':'role-id-2', 'name':'Name 2', 'ability':'some ability', 'team':'Townsfolk'},
             {'id':'role-id-3', 'name':'Name 3', 'ability':'some ability', 'team':'Townsfolk'},
             ]]
-        roles = lk.collect_roles_from_results(results_json)
+        roles = parser.collect_roles_from_json(results_json)
 
-        self.assertEqual(len(roles), 3, "Expected 3 roles")
+        self.assertEqual(len(roles), 3, 'Expected 3 roles')
 
 
     def test_coolectsroles_duperoles_createsone(self):
-        
-        lk = lookup.Lookup()
+        parser = lookup.LookupRoleParser()
 
         results_json = [[
             {'id':'role-id', 'name':'Name', 'ability':'some ability', 'team':'Townsfolk'},
             {'id':'role-id', 'name':'Name', 'ability':'some ability', 'team':'Townsfolk'},
             {'id':'role-id', 'name':'Name', 'ability':'some ability', 'team':'Townsfolk'},
             ]]
-        roles = lk.collect_roles_from_results(results_json)
+        roles = parser.collect_roles_from_json(results_json)
 
-        self.assertEqual(len(roles), 1, "Expected 1 role")
+        self.assertEqual(len(roles), 1, 'Expected 1 role')
+
+
+    def test_serverdatahasnothing_cantfindrole(self):
+        roles = {}
+        db = lookup.LookupRoleServerData(roles)
+
+        result = db.get_matching_roles('foo')
+
+        self.assertIsNone(result, 'Expected no results')
+
+
+    def test_serverdatahasroles_findsperfectmatch(self):
+        role = lookup.LookupRole('Some Role', 'Some Ability', 'Townsfolk')
+        roles = {role.name : [role]}
+        db = lookup.LookupRoleServerData(roles)
+
+        result = db.get_matching_roles('Some Role')
+
+        self.assertEqual(result[0], role, 'Expected matching role')
+
+
+    def test_serverdatahasroles_findsclosematch(self):
+        role = lookup.LookupRole('Some Role', 'Some Ability', 'Townsfolk')
+        roles = {role.name : [role]}
+        db = lookup.LookupRoleServerData(roles)
+
+        result = db.get_matching_roles('Som')
+
+        self.assertEqual(result[0], role, 'Expected matching role')
+
+
+    def test_serverdatahasroles_doesntfindnomatch(self):
+        role = lookup.LookupRole('Some Role', 'Some Ability', 'Townsfolk')
+        roles = {role.name : [role]}
+        db = lookup.LookupRoleServerData(roles)
+
+        result = db.get_matching_roles('bwuh')
+        
+        self.assertIsNone(result, 'Expected no results')
+
+
+    def test_serverdatahasroles_matchesrealisticmistake(self):
+        role = lookup.LookupRole('Boom-dandy', 'Some Ability', 'Townsfolk')
+        roles = {role.name : [role]}
+        db = lookup.LookupRoleServerData(roles)
+
+        result = db.get_matching_roles('bomdand')
+        
+        self.assertEqual(result[0], role, 'Expected matching role')
 
 
 if __name__ == '__main__':
