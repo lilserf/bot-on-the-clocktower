6b3668e5dec4e8a530cbeaffe729fced4a98abad
Mark Haines <lilserf@gmail.com>
2021-09-06 21:56:19 -0400

    Beginnings of announcement structures

---
+++ announce.py
@@ -0,0 +1,28 @@
+﻿
+
+class IAnnouncerDb:
+	def has_guild_seen_version(self, guild, version):
+		pass
+
+class IAnnouncerGuildDb:
+	def get_guilds():
+		pass
+
+class IAnnouncerVersionProvider:
+	def get_version_message(self, version):
+		pass
+
+	def get_latest_version(self):
+		pass
+
+	def get_latest_version_message(self):
+		return self.get_version_message(self.get_latest_version())
+
+class Announcer:
+
+	def __init__(self, db, guildDb, provider):
+		self.db = db
+		self.guildDb = guildDb
+		self.provider = provider
+
+
