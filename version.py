678c0388ab232ed6a3d50c3e2359ed475d509d4e
Mark Haines <lilserf@gmail.com>
2021-09-07 12:04:51 -0400

    Added bot name to the version announcement

--- version.py
+++ version.py
@@ -10,7 +10,7 @@ class VersionProviderImpl(IVersionProvider):
 		self.map = {};
 
 		# Version 2.0.0
-		v2 = discord.Embed(title="New features in v2.0.0!")
+		v2 = discord.Embed(title="New features in Bot on the Clocktower v2.0.0!")
 		
 		v2.add_field(inline=False, name="Vote Timer", value="You can now use `!votetimer 5m30s` to start a timer for the requested time. Villagers will be warned as the timer gets close, and when it expires, villagers will be pulled to the Town Square channel for a vote as if `!vote` was run.")
 		v2.add_field(inline=False, name="Character Lookup", value="The new `!character <name>` command fetches information about a given character from the official sets. You can also use `!addScript <json url>` to tell the bot about JSON script files for your own custom sets. If you're using [Bloodstar Clocktica](https://www.bloodstar.xyz) to manage your scripts, you'll even get links to the almanac!")
