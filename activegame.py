2fdb90556a0b7ced3775cfedfa2c0320ec673165
Colin C <magroader@gmail.com>
2021-09-10 10:38:34 -0400

    Setting up framework for active game storage, along with tests

---
+++ activegame.py
@@ -0,0 +1,30 @@
+﻿from botctypes import TownId
+from datetime import datetime
+
+class ActiveGame:
+    '''Class containing info about an in-progress game'''
+    def __init__(self):
+        self.town_id: TownId = None
+        self.last_activity: None
+        self.storyteller_ids: list[int] = []
+        self.player_ids: list[int] = []
+
+class IActiveGameStore:
+    '''Interface for a class that stores currently-active games'''
+
+    def get_active_game_count(self) -> int:
+        '''Return count of currently active games'''
+
+class IActiveGameDb:
+    '''Interface for a class that stores active game information in the database'''
+
+    def retrieve_active_games(self) -> list[ActiveGame]:
+        '''Retrieves all active games from the DB'''
+
+class ActiveGameStore(IActiveGameStore):
+    def __init__(self, active_game_db:IActiveGameDb):
+        self.db: IActiveGameDb = active_game_db
+        self.current_games: list[ActiveGame] = active_game_db.retrieve_active_games()
+    
+    def get_active_game_count(self) -> int:
+        return len(self.current_games)
---
+++ test_activegame.py
@@ -0,0 +1,30 @@
+﻿import activegame
+import unittest
+
+
+class TestActiveGameSync(unittest.TestCase):
+
+    def test_construct_active_game_no_games(self):
+        store = activegame.ActiveGameStore(MockActiveGameDb())
+
+        self.assertEqual(0, store.get_active_game_count())
+
+    def test_construct_active_game_with_games(self):
+        store1 = activegame.ActiveGameStore(MockActiveGameDb([activegame.ActiveGame()]))
+        store2 = activegame.ActiveGameStore(MockActiveGameDb([activegame.ActiveGame(), activegame.ActiveGame()]))
+
+        self.assertEqual(1, store1.get_active_game_count())
+        self.assertEqual(2, store2.get_active_game_count())
+
+
+
+class MockActiveGameDb(activegame.IActiveGameDb):
+    def __init__(self, games:list[activegame.ActiveGame]=[]):
+        self.games: list[activegame.ActiveGame] = games
+
+    def retrieve_active_games(self) -> list[activegame.ActiveGame]:
+        return self.games
+
+
+if __name__ == '__main__':
+    unittest.main()
