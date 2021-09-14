e1fc49f86128f21116a18c073941662e3e1b6544
Colin C <magroader@gmail.com>
2021-09-14 12:07:58 -0400

    Working on new timedcallback system to request periodic callbacks

---
+++ test_timedcallback.py
@@ -0,0 +1,63 @@
+﻿# pylint: disable=missing-class-docstring, disable=missing-function-docstring, disable=missing-module-docstring, disable=invalid-name, disable=wildcard-import, disable=unused-wildcard-import
+
+import unittest
+from datetime import timedelta
+
+from timedcallback import *
+
+class TestTimedCallbackManagerFactory(unittest.TestCase):
+    def setUp(self):
+        self.mock_loop_factory:MockLoopFactory = MockLoopFactory()
+        self.factory:TimedCallbackManagerFactory = TimedCallbackManagerFactory(self.mock_loop_factory)
+
+    def test_construct_no_loops(self):
+        self.assertEqual(0, self.mock_loop_factory.create_loop_count)
+
+    def test_request_loop_creates(self):
+        manager = self.factory.get_timed_callback_manager(None, timedelta(hours=1))
+
+        self.assertEqual(1, self.mock_loop_factory.create_loop_count)
+        self.assertEqual(60*60, self.mock_loop_factory.create_loop_seconds)
+        self.assertIsInstance(manager, TimedCallbackManager)
+
+    def test_request_loop_twice_creates_two(self):
+        manager1 = self.factory.get_timed_callback_manager(None, timedelta(hours=1))
+        manager2 = self.factory.get_timed_callback_manager(None, timedelta(seconds=1))
+
+        self.assertEqual(2, self.mock_loop_factory.create_loop_count)
+        self.assertEqual(1, self.mock_loop_factory.create_loop_seconds)
+        self.assertIsInstance(manager2, TimedCallbackManager)
+        self.assertNotEqual(manager1, manager2)
+
+    def test_request_loop_same_time_creates_one(self):
+        manager1 = self.factory.get_timed_callback_manager(None, timedelta(hours=1))
+        manager2 = self.factory.get_timed_callback_manager(None, timedelta(hours=1))
+
+        self.assertEqual(manager1, manager2)
+
+class TestTimedCalbackManager(unittest.TestCase):
+    # TODO tests for manager
+    pass
+
+
+class MockLoop(ILoop):
+    def __init__(self):
+        self.start_calls:int = 0
+        self.stop_calls:int = 0
+
+    def start(self):
+        self.start_calls += 1
+
+    def stop(self):
+        self.stop_calls += 1
+
+class MockLoopFactory(ILoopFactory):
+    def __init__(self):
+        self.create_loop_count:int = 0
+        self.create_loop_callback:Callable[[None], None] = 0
+        self.create_loop_seconds:int = 0
+
+    def create_loop(self, callback:Callable[[None], None], seconds:int) -> ILoop:
+        self.create_loop_count += 1
+        self.create_loop_callback = callback
+        self.create_loop_seconds = seconds
---
+++ timedcallback.py
@@ -0,0 +1,71 @@
+﻿'''Classes for creating callbacks that happen at particular times'''
+
+from datetime import datetime, timedelta
+from typing import Callable
+
+from discord.ext.tasks import Loop
+
+class ITimedCallbackManager:
+    '''Object that checks for timeouts every so often and calls a callback when they hit'''
+
+    def create_or_update_request(self, key:object, calltime:datetime) -> None:
+        '''Creates or updates a callback request with a given key to be called at a particular time'''
+
+    def remove_request(self, key:object) -> None:
+        '''Removes a request for a callback at a particular time'''
+
+class ITimedCallbackManagerFactory:
+    '''Factory for creating ITimedCallbackManagers'''
+
+    def get_timed_callback_manager(self, callback:Callable[[object], None], check_delta:timedelta) -> ITimedCallbackManager:
+        '''Returns an ITimedCallbackManager for calling callbacks and checking every timespan. The callback will take the time.'''
+
+class ILoop():
+    '''Interface for a loop, matches API of Loop class in discord.ext'''
+
+    def start(self):
+        '''Starts the loop running'''
+
+    def stop(self):
+        '''Stops the loop running'''
+
+class ILoopFactory:
+    '''Factory for creating Loop objects'''
+
+    def create_loop(self, callback:Callable[[None], None], seconds:int) -> ILoop:
+        '''Creates a Loop object with the given times'''
+
+
+class TimedCallbackManager(ITimedCallbackManager):
+
+    def create_or_update_request(self, key:object, calltime:datetime) -> None:
+        pass
+
+    def remove_request(self, key:object) -> None:
+        pass
+
+class TimedCallbackManagerFactory(ITimedCallbackManagerFactory):
+
+    def __init__(self, loop_factory:ILoopFactory):
+        self.loop_factory:ILoopFactory = loop_factory
+        self.manager_lookup:dict[timedelta, TimedCallbackManager] = {}
+
+    def get_timed_callback_manager(self, callback:Callable[[object], None], check_delta:timedelta) -> ITimedCallbackManager:
+        self.loop_factory.create_loop(None, check_delta.total_seconds())
+
+        if check_delta not in self.manager_lookup:
+            self.manager_lookup[check_delta] = TimedCallbackManager()
+        return self.manager_lookup[check_delta]
+
+class LoopFactory(ILoopFactory):
+    '''Implementation of ILoopFactory'''
+
+    def create_loop(self, callback:Callable[[None], None], seconds:int) -> None:
+        return Loop(
+            callback,
+            seconds=seconds,
+            minutes=0,
+            hours=0,
+            count=None,
+            reconnect=True,
+            loop=None)
