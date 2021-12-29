3f77e4800d107a1c2227da84c0ca82e6b154ccb4
Colin C <magroader@gmail.com>
2021-12-29 11:46:57 -0500

    Slash command now static

--- Bot.DSharp/DSharpGameSlashCommands.cs
+++ Bot.DSharp/DSharpGameSlashCommands.cs
@@ -8,6 +8,6 @@ namespace Bot.DSharp
     internal class DSharpGameSlashCommands : SlashCommandModule
     {
         [SlashCommand("game", "Starts up a game of Blood on the Clocktower")]
-        public Task GameCommand(InteractionContext ctx) => ctx.Services.GetService<IBotGameService>().RunGameAsync(new DSharpInteractionContext(ctx));
+        public static Task GameCommand(InteractionContext ctx) => ctx.Services.GetService<IBotGameService>().RunGameAsync(new DSharpInteractionContext(ctx));
     }
 }
