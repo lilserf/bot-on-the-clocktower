b4b0a680c3e7e106b2a86d835b785f7066bf0d9d
Mark Haines <lilserf@gmail.com>
2021-12-29 22:23:23 -0500

    Initial projects for Bot.Database

---
+++ Bot.Database/BotDatabase.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Bot.Database
+{
+	public class BotDatabase
+	{
+	}
+}
---
+++ Test.Bot.Database/TestDatabase.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Test.Bot.Database
+{
+	public class TestDatabase
+	{
+		[Fact]
+		public void Test1()
+		{
+
+		}
+	}
+}
