eebf97d395de79b1e3505af80e9b99839d976957
Mark Haines <lilserf@gmail.com>
2021-12-30 23:28:40 -0500

    Whoops, test code commented back out

--- Bot.Main/Program.cs
+++ Bot.Main/Program.cs
@@ -22,14 +22,14 @@ namespace Bot.Main
             DatabaseFactory dbp = new(sp);
             sp = dbp.Connect();
 
-			// TEST CODE
-			// Demonstrates that we can successfully get a Town's details from the ITownLookup service
-			ITownLookup itl = sp.GetService<ITownLookup>();
-            ITown t = await itl.GetTown(128585855097896963, 826858511438839879);
-            Console.WriteLine(t);
-			// END TEST CODE
+            //// TEST CODE
+            //// Demonstrates that we can successfully get a Town's details from the ITownLookup service
+            //ITownLookup itl = sp.GetService<ITownLookup>();
+            //ITown t = await itl.GetTown(128585855097896963, 826858511438839879);
+            //Console.WriteLine(t);
+            //// END TEST CODE
 
-			DSharpSystem dSharpSystem = new();
+            DSharpSystem dSharpSystem = new();
             BotSystemRunner botRunner = new(sp, dSharpSystem);
 
             await botRunner.RunAsync(CancellationToken.None);
