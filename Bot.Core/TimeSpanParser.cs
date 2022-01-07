3e642aba4536a096141796c3939643ac092e1d09
Colin C <magroader@gmail.com>
2022-01-06 21:02:36 -0500

    TimeSpanParser and associated tests

---
+++ Bot.Core/TimeSpanParser.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Bot.Core
+{
+    public static class TimeSpanParser
+    {
+        private static readonly Regex s_minRegex = new(@"(\d+)(?:\s*)(?:minutes|minute|mins|min|m)\s*,?");
+        private static readonly Regex s_secRegex = new(@"(\d+)(?:\s*)(?:seconds|second|secs|sec|s)\s*,?");
+
+        public static TimeSpan? Parse(string time)
+        {
+            (int minutes, Match? minMatch) = FindRegexMatch(s_minRegex, time);
+            (int seconds, Match? secMatch) = FindRegexMatch(s_secRegex, time);
+
+            if (minutes == 0 && seconds == 0)
+                return null;
+
+            if (!DoRegexMatchesCoverEntireString(time, minMatch, secMatch))
+                return null;
+
+            return TimeSpan.FromMinutes(minutes) + TimeSpan.FromSeconds(seconds);
+        }
+
+        private static (int, Match?) FindRegexMatch(Regex regex, string str)
+        {
+            int value = 0;
+
+            var matches = regex.Matches(str);
+            if (matches.Count > 1)
+                return (0, null);
+
+            var match = matches.FirstOrDefault();
+            if (match != null)
+                if (!int.TryParse(match.Groups[1].Value, out value))
+                    return (0, null);
+
+            return (value, match);
+        }
+
+        private static bool DoRegexMatchesCoverEntireString(string str, params Match?[] matches)
+        {
+            foreach (var match in matches)
+                if (match != null)
+                    str = str.Remove(str.IndexOf(match.Value), match.Length);
+
+            return string.IsNullOrWhiteSpace(str);
+        }
+    }
+}
---
+++ Test.Bot.Core/TestTimeSpanParser.cs
@@ -0,0 +1,99 @@
+﻿using Bot.Core;
+using System;
+using Xunit;
+
+namespace Test.Bot.Core
+{
+    public static class TestTimeSpanParser
+    {
+        [Theory]
+        [InlineData("")]
+        [InlineData("m")]
+        [InlineData("min")]
+        [InlineData("minutes")]
+        [InlineData("s")]
+        [InlineData("sec")]
+        [InlineData("seconds")]
+        [InlineData("1")]
+        [InlineData("01")]
+        [InlineData(".")]
+        [InlineData("0m")]
+        public static void TimeSpanParser_InvalidOptions(string test)
+        {
+            var parsedTimeSpan = TimeSpanParser.Parse(test);
+            Assert.Null(parsedTimeSpan);
+        }
+
+        [Theory]
+        [InlineData(1, "1 m")]
+        [InlineData(2, "2 min")]
+        [InlineData(3, "3 mins")]
+        [InlineData(4, "4 minute")]
+        [InlineData(5, "5 minutes")]
+        [InlineData(5, "5m")]
+        [InlineData(4, "4min")]
+        [InlineData(3, "3mins")]
+        [InlineData(2, "2minute")]
+        [InlineData(1, "1minutes")]
+        [InlineData(10, "10m")]
+        public static void TimeSpanParser_SimpleMinutes(int mins, string test)
+        {
+            var expectedTimeSpan = TimeSpan.FromMinutes(mins);
+            var parsedTimeSpan = TimeSpanParser.Parse(test);
+            Assert.Equal(expectedTimeSpan, parsedTimeSpan);
+        }
+
+        [Theory]
+        [InlineData("-1m")]
+        [InlineData("1m 2m")]
+        [InlineData("1m2minutes")]
+        [InlineData("1m 2min")]
+        public static void TimeSpanParser_InvalidMinutes(string test)
+        {
+            var parsedTimeSpan = TimeSpanParser.Parse(test);
+            Assert.Null(parsedTimeSpan);
+        }
+
+        [Theory]
+        [InlineData(1, "1 s")]
+        [InlineData(2, "2 sec")]
+        [InlineData(3, "3 secs")]
+        [InlineData(4, "4 second")]
+        [InlineData(5, "5 seconds")]
+        [InlineData(5, "5s")]
+        [InlineData(4, "4sec")]
+        [InlineData(3, "3secs")]
+        [InlineData(2, "2second")]
+        [InlineData(1, "1seconds")]
+        [InlineData(10, "10s")]
+        public static void TimeSpanParser_SimpleSeconds(int secs, string test)
+        {
+            var expectedTimeSpan = TimeSpan.FromSeconds(secs);
+            var parsedTimeSpan = TimeSpanParser.Parse(test);
+            Assert.Equal(expectedTimeSpan, parsedTimeSpan);
+        }
+
+        [Theory]
+        [InlineData("-1s")]
+        [InlineData("1s 2s")]
+        [InlineData("1s2seconds")]
+        [InlineData("1s 2sec")]
+        public static void TimeSpanParser_InvalidSeconds(string test)
+        {
+            var parsedTimeSpan = TimeSpanParser.Parse(test);
+            Assert.Null(parsedTimeSpan);
+        }
+
+        [Theory]
+        [InlineData(1, 2, "1m2s")]
+        [InlineData(5, 30, "30 sec, 5 mins")]
+        [InlineData(2, 0, "120 sec")]
+        [InlineData(1, 45, "1 minute    45 seconds")]
+        public static void TimeSpanParser_MinsAndSecs(int mins, int secs, string test)
+        {
+            var expectedTimeSpan = TimeSpan.FromMinutes(mins) + TimeSpan.FromSeconds(secs);
+            var parsedTimeSpan = TimeSpanParser.Parse(test);
+            Assert.Equal(expectedTimeSpan, parsedTimeSpan);
+        }
+    }
+}
