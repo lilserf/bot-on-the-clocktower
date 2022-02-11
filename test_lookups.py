eae29d8e3e0df0cc73ab879ef1e65addc337f9a0
Colin C <magroader@gmail.com>
2022-02-11 09:09:17 -0500

    Added second test for role lookup pointing at new wiki location

--- test_lookups.py
+++ test_lookups.py
@@ -195,9 +195,15 @@ class TestLookups(unittest.TestCase):
         self.assertEqual('unofficial_image_2', found_main_role.roles_in_script_infos[2].script_info.image)
 
     def test_botc_wiki_link_correct(self):
-        role = 'Washerwoman'
-        url = lookup.BotcWiki.create_wiki_url(role)
-        self.assertEqual(f'https://wiki.bloodontheclocktower.com/{role}', url)
+        role1 = 'Washerwoman'
+        role1_expected_url = 'Washerwoman'
+        role1_actual_url = lookup.BotcWiki.create_wiki_url(role1)
+        self.assertEqual(f'https://wiki.bloodontheclocktower.com/{role1_expected_url}', role1_actual_url)
+
+        role2 = 'Fortune Teller'
+        role2_expected_url = 'Fortune_Teller'
+        role2_actual_url = lookup.BotcWiki.create_wiki_url(role2)
+        self.assertEqual(f'https://wiki.bloodontheclocktower.com/{role2_expected_url}', role2_actual_url)
 
 
 class TestLookupImpl(unittest.IsolatedAsyncioTestCase):
