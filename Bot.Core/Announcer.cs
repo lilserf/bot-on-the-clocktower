edd45a9660951cdf94d5503d3cb655a836699f6b
Mark Haines <lilserf@gmail.com>
2022-04-04 21:22:36 -0400

    Add a mechanism to restrict announcements to only certain guilds during development

--- Bot.Core/Announcer.cs
+++ Bot.Core/Announcer.cs
@@ -11,6 +11,11 @@ namespace Bot.Core
 {
     public class Announcer : IAnnouncer
     {
+        static List<ulong> s_guildAllowList = new List<ulong>()
+        {
+            128585855097896963,
+        };
+
         IVersionProvider m_versionProvider;
         IAnnouncementDatabase m_announcementDatabase;
         ITownDatabase m_townDatabase;
@@ -29,7 +34,7 @@ namespace Bot.Core
             sp.Inject(out m_townDatabase);
             sp.Inject(out m_botSystem);
             sp.Inject(out m_botClient);
-            sp.Inject(out m_dateTime);
+            sp.Inject(out m_dateTime);            
 
             var callbackFactory = sp.GetService<ICallbackSchedulerFactory>();
             m_callbackScheduler = callbackFactory.CreateScheduler<Queue<TownKey>>(AnnounceToTowns, TimeSpan.FromMinutes(1));
@@ -44,18 +49,27 @@ namespace Bot.Core
 
         private async Task AnnounceToTowns(Queue<TownKey> towns)
         {
+            bool restricted = bool.Parse(Environment.GetEnvironmentVariable("RESTRICT_ANNOUNCE") ?? "false");
+
             Serilog.Log.Information("AnnounceToTowns: {townCount} towns remain...", towns.Count());
             int numSent = 0;
             while(numSent < NUM_TOWNS_PER_CALLBACK && towns.Count > 0)
             {
                 var townKey = towns.Dequeue();
+
+                // If we're restricted, skip all guilds not in the allowList
+                if (restricted && !s_guildAllowList.Contains(townKey.GuildId))
+                    continue;
+
                 Serilog.Log.Information("AnnounceToTowns: Checking town {townKey}", townKey);
 
                 var guild = await m_botClient.GetGuildAsync(townKey.GuildId);
-                if (guild == null) continue;
+                if (guild == null) 
+                    continue;
 
                 var chan = guild.GetChannel(townKey.ControlChannelId);
-                if (chan == null) continue;
+                if (chan == null) 
+                    continue;
 
                 foreach (var versionObj in m_versionProvider.Versions)
                 {
