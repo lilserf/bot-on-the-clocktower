8f0f48305e41d0bfd636583ce1ac2163a23e4c59
Colin C <magroader@gmail.com>
2022-04-09 13:53:11 -0400

    Making a simple change to see if the bot restarts

--- Bot.Core/Announcer.cs
+++ Bot.Core/Announcer.cs
@@ -1,10 +1,8 @@
 ﻿using Bot.Api;
 using Bot.Api.Database;
-using Bot.Core.Callbacks;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Bot.Core
