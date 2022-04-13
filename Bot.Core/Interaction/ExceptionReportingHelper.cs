7365a38dd5929ac8f9956573de30aa47ce544346
Colin C <magroader@gmail.com>
2022-04-13 13:58:10 -0400

    Exception reporter truncates stack trace if too long

--- Bot.Core/Interaction/ExceptionReportingHelper.cs
+++ Bot.Core/Interaction/ExceptionReportingHelper.cs
@@ -6,11 +6,15 @@ namespace Bot.Core.Interaction
 {
     public static class ExceptionReportingHelper
     {
+        private const int MaximumMessageLength = 2000; // Hardcoded in DSharpPlus
         public static async Task TrySendExceptionToMemberAsync(string identifier, IMember member, Exception e)
         {
             try
             {
-                await member.SendMessageAsync($"Bot on the Clocktower encountered an error.\n\nPlease consider reporting the error at <https://github.com/lilserf/bot-on-the-clocktower/issues> and including all the information below:\n\n{identifier}\nException: `{e.GetType().Name}`\nMessage:   `{e.Message}`\nStack trace:\n```{e.StackTrace}```");
+                string messageMinusStackTrace = $"Bot on the Clocktower encountered an error.\n\nPlease consider reporting the error at <https://github.com/lilserf/bot-on-the-clocktower/issues> and including all the information below:\n\n{identifier}\nException: `{e.GetType().Name}`\nMessage:   `{e.Message}`\nStack trace:\n";
+                int availableForStackTrace = MaximumMessageLength - messageMinusStackTrace.Length - 6;
+                string fullMessage = $"{messageMinusStackTrace}{(e.StackTrace == null ? "n/a" : availableForStackTrace > 0 ? $"```{e.StackTrace![..availableForStackTrace]}```" : "(2long)")}";
+                await member.SendMessageAsync(fullMessage[..MaximumMessageLength]);
             }
             catch (Exception)
             {
