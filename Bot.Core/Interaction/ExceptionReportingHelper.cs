fecaa97999aa2f783f7f1e48d01d815d81f585e8
Colin C <magroader@gmail.com>
2022-04-13 14:19:30 -0400

    Fixing bugs with the range operator

--- Bot.Core/Interaction/ExceptionReportingHelper.cs
+++ Bot.Core/Interaction/ExceptionReportingHelper.cs
@@ -13,12 +13,17 @@ namespace Bot.Core.Interaction
             {
                 string messageMinusStackTrace = $"Bot on the Clocktower encountered an error.\n\nPlease consider reporting the error at <https://github.com/lilserf/bot-on-the-clocktower/issues> and including all the information below:\n\n{identifier}\nException: `{e.GetType().Name}`\nMessage:   `{e.Message}`\nStack trace:\n";
                 int availableForStackTrace = MaximumMessageLength - messageMinusStackTrace.Length - 6;
-                string fullMessage = $"{messageMinusStackTrace}{(e.StackTrace == null ? "n/a" : availableForStackTrace > 0 ? $"```{e.StackTrace![..availableForStackTrace]}```" : "(2long)")}";
-                await member.SendMessageAsync(fullMessage[..MaximumMessageLength]);
+                string fullMessage = $"{messageMinusStackTrace}{(e.StackTrace == null ? "n/a" : availableForStackTrace > 0 ? $"```{GetRangeOrFull(e.StackTrace!, availableForStackTrace)}```" : "(2long)")}";
+                await member.SendMessageAsync(GetRangeOrFull(fullMessage, MaximumMessageLength));
             }
             catch (Exception)
             {
                 // Not a whole lot we can do about this. Maybe log it?
+            }
+
+            string GetRangeOrFull(string str, int max)
+            {
+                return max < str.Length ? str[..max] : str;
             }
         }
     }
