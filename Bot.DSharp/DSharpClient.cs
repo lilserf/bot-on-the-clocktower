efb42ec9dfaef78909f61c1db76d842e29db1f8b
Mark Haines <lilserf@gmail.com>
2022-05-04 11:05:31 -0400

    Cleaner implementation of slash command registration

--- Bot.DSharp/DSharpClient.cs
+++ Bot.DSharp/DSharpClient.cs
@@ -11,8 +11,8 @@ namespace Bot.DSharp
     public class DSharpClient : IBotClient
     {
         private readonly IComponentService m_componentService;
-        private readonly IEnvironment m_environment;
         private readonly IDiscordClient m_discord;
+        private readonly string m_deployType;
 
         public event EventHandler<EventArgs>? Connected;
         public event EventHandler<MessageCreatedEventArgs>? MessageCreated;
@@ -20,9 +20,9 @@ namespace Bot.DSharp
         public DSharpClient(IServiceProvider serviceProvider)
         {
             serviceProvider.Inject(out m_componentService);
-            serviceProvider.Inject(out m_environment);
 
             var environment = serviceProvider.GetService<IEnvironment>();
+            m_deployType = environment.GetEnvironmentVariable("DEPLOY_TYPE") ?? "none";
             var token = environment.GetEnvironmentVariable("DISCORD_TOKEN");
             if (string.IsNullOrWhiteSpace(token)) throw new InvalidDiscordTokenException();
 
@@ -37,41 +37,50 @@ namespace Bot.DSharp
             m_discord = discordFactory.CreateClient(config);
         }
 
+        private void RegisterCommands(SlashCommandsExtension slash, IEnumerable<Type> commands, ulong? guildId = null)
+        {
+            foreach (var t in commands)
+            {
+                slash.RegisterCommands(t, guildId);
+            }
+        }
+
         public async Task ConnectAsync(IServiceProvider botServices)
         {
             var slash = m_discord.UseSlashCommands(new SlashCommandsConfiguration { Services = botServices });
 
-            // If somehow neither deploy type is specified, don't screw around with anything
-            string deployType = Environment.GetEnvironmentVariable("DEPLOY_TYPE") ?? "none";
-
-            // The "dev" deploy should only be used by us doing development locally and using the 
-            // DEV bot token - so the DEV bot will remain registered only to our servers, with no global
-            // commands.
-            // The "prod" deploy should only be used by the real server using the real bot token,
-            // so it will now be registered globally
-            if (deployType == "dev")
+            var commandTypes = new[]
             {
-                // During development, register our commands to the dev guild only
+                typeof(DSharpGameSlashCommands),
+                typeof(DSharpMessagingSlashCommands),
+                typeof(DSharpLookupSlashCommands),
+                typeof(DSharpMiscSlashCommands),
+                typeof(DSharpSetupSlashCommands),
+            };
+
+            if (m_deployType == "dev")
+            {
+                // The "dev" deploy should only be used by us doing development locally and using the 
+                // DEV bot token - so the DEV bot will remain registered only to our servers, with no global
+                // commands.
                 List<ulong> devGuildIds = new() { 128585855097896963ul, 215551375608643586ul };
 
                 foreach (ulong devGuildId in devGuildIds)
                 {
-                    slash.RegisterCommands<DSharpGameSlashCommands>(devGuildId);
-                    slash.RegisterCommands<DSharpMessagingSlashCommands>(devGuildId);
-                    slash.RegisterCommands<DSharpLookupSlashCommands>(devGuildId);
-                    slash.RegisterCommands<DSharpMiscSlashCommands>(devGuildId);
-                    slash.RegisterCommands<DSharpSetupSlashCommands>(devGuildId);
+                    RegisterCommands(slash, commandTypes, devGuildId);
                 }
                 // During development, register no commands globally
                 slash.RegisterCommands<EmptyCommands>();
             }
-            else if(deployType == "prod")
+            else if (m_deployType == "prod")
             {
-                slash.RegisterCommands<DSharpGameSlashCommands>();
-                slash.RegisterCommands<DSharpMessagingSlashCommands>();
-                slash.RegisterCommands<DSharpLookupSlashCommands>();
-                slash.RegisterCommands<DSharpMiscSlashCommands>();
-                slash.RegisterCommands<DSharpSetupSlashCommands>();
+                // The "prod" deploy should only be used by the real server using the real bot token,
+                // so it will now be registered globally
+                RegisterCommands(slash, commandTypes);
+            }
+            else
+            {
+                throw new InvalidOperationException("Bot must be configured with either 'dev' or 'prod' DEPLOY_TYPE!");
             }
 
             TaskCompletionSource readyTcs = new();
